03443ca521e5387b91ca046c932f9734e1f6b421
prasan1h <prasanna.k.h.070@gmail.com>
2026-02-05 21:20:24 +0530

    paste btn

--- copy-paste/index.js
+++ copy-paste/index.js
@@ -1,16 +1,22 @@
 const txt = document.getElementById('content');
 const op = document.getElementById('output');
 const btn = document.getElementById('btn');
+const btnp = document.getElementById('btnp');
 const dataOp = document.getElementById('data-output');
 const paste = document.getElementById('paste');
 var data = '';
 
 txt.onchange = function() {
     data = this.value;
-    window.navigator.clipboard.readText(data)
 }
 
 btn.onclick = function() {
     console.log("d : ",data);
     window.navigator.clipboard.writeText(data);
+}
+
+btnp.onclick = function() {
+   navigator.clipboard.readText(data).then(txt => {
+        paste.value = data;
+    });
 }
